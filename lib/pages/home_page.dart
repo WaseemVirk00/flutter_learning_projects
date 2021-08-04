0d309f7659c90ee22c0879867faa6b77a673dc19
WaseemVirk00 <48316969+WaseemVirk00@users.noreply.github.com>
2021-08-04 17:12:04 +0500

    code updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/change_name_mycard.dart';
-import 'package:hello_world/drawer.dart';
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -12,7 +9,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
   var myText = "Change me";
-
   @override
   void initState() {
     super.initState();
@@ -30,18 +26,71 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Card(
-            child: change_name_mycard(myText: myText, nameController: _nameController),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets/images/laptop.jpg",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: "Enter Something here",
+                      labelText: "Name",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
-      drawer: MyDrawer(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Waceem Virk"),
+              accountEmail: Text("waseemvirk00@gmail.com"),
+              decoration: BoxDecoration(color: Colors.purple),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage:
+                NetworkImage("https://twitter.com/waceem_/photo"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           myText = _nameController.text;
-          setState(() {});
+          setState(() {
+
+          });
         },
         child: Icon(Icons.refresh),
       ),
     );
   }
 }
+
