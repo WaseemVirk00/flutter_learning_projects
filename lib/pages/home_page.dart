d46844bca484a31bd7b3792bfb2199e0f7c24e15
WaseemVirk00 <48316969+WaseemVirk00@users.noreply.github.com>
2021-08-04 16:54:42 +0500

    code updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,47 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/change_name_mycard.dart';
-import 'package:hello_world/drawer.dart';
 
-class HomePage extends StatefulWidget {
+
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  TextEditingController _nameController = TextEditingController();
-  var myText = "Change me";
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("Hello World"),
+        title: Text("Awesome App"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: change_name_mycard(myText: myText, nameController: _nameController),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: Text(
+            "I am \na Box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
           ),
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 5, offset: Offset(2.0, 5.0))
+              ]),
         ),
-      ),
-      drawer: MyDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.refresh),
       ),
     );
   }
 }
+
